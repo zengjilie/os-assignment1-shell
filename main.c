a683b8a73a0d5054fb9514ab45e3b77239cdf4a1
zengjilie <zengjilie@gmail.com>
2022-01-25 10:03:35 -0600

    ..

--- main.c
+++ main.c
@@ -8,21 +8,41 @@
 //     return 0;
 // }
 
-//FORK
-// int main(){
-//     pid_t pid;
-//     pid = fork();
-//     printf("%d\n", pid);
-
-//     return 0;
-// }
-int main()
-{
-    int x = 3;
-    if (fork() == 0)
-    {
-        printf("C: x=%d;", --x);
+// FORK
+int main(){
+    pid_t pid;
+    pid = fork();
+    if(pid == 0){
+        printf("I am a child %d\n", pid);
+    }else{
+        printf("I am a parent %d\n" , pid);
     }
-    printf("P: x=%d;", ++x);
-    exit(0);
+
+    return 0;
+}
+
+// int main()
+// {
+//     int x = 3;
+//     if (fork() == 0)
+//     {
+//         printf("C: x=%d;", --x);
+//     }
+//     printf("P: x=%d;", ++x);
+//     exit(0);
+// }
+
+//
+int main(){
+    int cpid;
+    char * inString;
+
+    while(inString = readline("cmd:")){
+        cpid = fork();
+        if(cpid == 0){
+            execlp(inString, inString, (char*)NULL);
+        }else{
+            wait((int *)NULL);
+        }
+    }
 }
