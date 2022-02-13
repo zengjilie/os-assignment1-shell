de12d0ce4a3d6f3f53e2af5a2542a40f466028b5
zengjilie <zengjilie@gmail.com>
2022-02-12 18:58:47 -0600

    finished redirection

--- do.c
+++ do.c
@@ -1,33 +1,75 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <dirent.h>
 
 int main(int argc, char *argv[])
 {
-    // fgets(input, 100, stdin);
-    // input[strlen(input) - 1] = '\0';
-
-    // char *strp[10];
-    char string[100];
-    fgets(string, 100, stdin);
-    string[strlen(string)-1] = '\0';
-
-    char *piece = strtok(string, " ");
-    // int i = 0;
-    while (piece != NULL)
+    FILE *fPointer;
+    fPointer = fopen("text.txt", "r");
+    char singleLine[150];
+    if (fPointer == NULL) // error handling
     {
-        // args[i] = piece;
-        printf("%s\n", piece);
-        piece = strtok(NULL, " ");
-        // i++;
+        printf("No such file in current directory");
     }
-    // for (int i = 0; i < sizeof strp / sizeof strp[0]; i++)
+    else
+    {
+        while (!feof(fPointer))
+        {
+            fgets(singleLine, 150, fPointer);
+            printf("%s", singleLine);
+        }
+        fclose(fPointer);
+    }
+    // int id = -1;
+    // if (id != 0)
     // {
-    //     if (strp[i] == NULL)
-    //     {
-    //         break;
-    //     }
-    //     printf("%s\n", strp[i]);
+    //     printf("# ");
     // }
+
+    // id = fork();
+    // if (id == 0)
+    // {
+    //     printf("child");
+    // }
+    // else
+    // {
+    //     // wait(NULL);
+    //     printf("parent");
+    // }
+    // char input[100];
+    // fgets(input, 100, stdin);
+    // int id = fork();
+    // if(id == 0){
+    //     printf("%s",input);
+    // }else{
+    //     wait(NULL);
+    //     printf("gege%s",input);
+    // }
+    // DIR *dir = opendir(".");
+    // if (dir == NULL)
+    // {
+    //     return 1;
+    // }
+
+    // struct dirent *entity;
+    // entity = readdir(dir);
+    // while (entity != NULL)
+    // {
+    //     printf("%s\t", entity->d_name);
+    //     entity = readdir(dir);
+    // }
+
+    // int file = open("text.txt", O_WRONLY);
+    // dup2(file, STDOUT_FILENO);
+    // // printf("\n");
+    // closedir(dir);
+    // int file = open(input, O_CREAT | O_WRONLY);
+
+    // dup2 (file, STDOUT_FILENO);
+    // printf("this is good");
+    // // printf("%d",isSame);
     return 0;
 }
--- main.c
+++ main.c
@@ -1,47 +1,86 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
 #include <fcntl.h>
 #include <string.h>
+#include <dirent.h>
 
-// fileDirection module
-int redirect(char input[])
+// ls
+int listFiles()
 {
-    // Seperate by whitespace
-    char *args[1000]; //"string array"
-    char *piece = strtok(input, " ");
-    int i = 0;
-    while (piece != NULL)
+    DIR *dir = opendir(".");
+    if (dir == NULL)
     {
-        args[i] = piece;
-        piece = strtok(NULL, " ");
-        i++;
-    }
-    args[i] = NULL; // where to stop
-
-    // looping through the command piece
-    for (int j = 0; j < sizeof args / sizeof args[0]; j++)
-    {
-        if (args[j] == NULL)
-        {
-            break;
-        }
-        // printf("%s\n", args[j]);
-        // encounter >  or < -> check if it has stdin && stdout
-
-        // if (strcmp(args[j], ">") == 0)
-        // {
-        //     printf("found >\n");
-        // }
-        // if (strcmp(args[j], "<") == 0)
-        // {
-        //     printf("found <\n");
-        // }
-
+        return 1;
     }
 
+    struct dirent *entity;
+    entity = readdir(dir);
+    while (entity != NULL)
+    {
+        printf("%s\t", entity->d_name);
+        entity = readdir(dir);
+    }
+
+    // printf("\n");
+    closedir(dir);
     return 0;
 }
+
+// cat
+
+// fileDirection module
+// int redirect(char *args[])
+// {
+//     // looping through the args
+//     for (int j = 0; j < sizeof args / sizeof args[0]; j++)
+//     {
+//         if (args[j] == NULL)
+//         {
+//             break;
+//         }
+//         // printf("%s\n", args[j]);
+//         // check operator
+//         int isGreater = strcmp(args[j], ">");
+//         int isSmaller = strcmp(args[j], "<");
+//         int isTwoGreater = strcmp(args[j], "2>");
+
+//         if (isGreater == 0)
+//         {
+//             if (args[j - 1] != NULL && args[j + 1] != NULL) //_ > _
+//             {
+//                 int file = open(args[j], O_CREAT | O_WRONLY | O_TRUNC);
+//             }
+//             else
+//             {
+//                 break;
+//             }
+//         }
+//         else if (isSmaller == 0)
+//         {
+//             if (args[j - 1] != NULL && args[j + 1] != NULL)
+//             {
+//             }
+//             else
+//             {
+//                 break;
+//             }
+//         }
+//         else if (isTwoGreater == 0)
+//         {
+//             if (args[j - 1] != NULL && args[j + 1] != NULL)
+//             {
+//             }
+//             else
+//             {
+//                 break;
+//             }
+//         }
+//     }
+
+//     return 0;
+// }
 
 // pipe
 // int pipe()
@@ -52,31 +91,161 @@ int main()
 {
     while (1)
     {
-        // yash shell
-        printf("yash$ ");
-
-        // take user input
+        // Start
+        printf("# ");
+        // User input
         char input[100];
         fgets(input, 100, stdin);
         input[strlen(input) - 1] = '\0';
 
-        // input parsing
-
-        // Redirection
-        int isRedirection = 0;
-        for (int i = 0; i < strlen(input); i++)
+        // Parsing input args
+        char *args[100]; //"string array"
+        char *piece = strtok(input, " ");
+        int i = 0;
+        while (piece != NULL)
         {
-            if (input[i] == '>' || input[i] == '<')
+            args[i] = piece;
+            piece = strtok(NULL, " ");
+            i++;
+        }
+        args[i] = NULL; // manually add null
+
+        // checking all args
+
+        int id = fork();
+        if (id == 0)
+        {
+            for (int j = 0; j < sizeof args / sizeof args[0]; j++)
             {
-                isRedirection = 1;
+                if (args[j] == NULL) // stop
+                {
+                    break;
+                }
+                else if (strcmp(args[j], "ls") == 0) // list files
+                {
+                    // hasLs = 1;
+                    listFiles();
+                }
+                else if (strcmp(args[j], "cat") == 0)// jump over <
+                {
+                    FILE *fPointer;
+                    if (strcmp(args[j + 1], "<") == 0)
+                    {
+                        fPointer = fopen(args[j + 2], "r");
+                        j++;
+                    }else{
+                        fPointer = fopen(args[j + 1], "r");
+                    }
+                    char singleLine[150];
+                    if (fPointer == NULL) // error handling
+                    {
+                        printf("No such file in current directory");
+                    }
+                    else
+                    {
+                        while (!feof(fPointer))
+                        {
+                            fgets(singleLine, 150, fPointer);
+                            printf("%s", singleLine);
+                        }
+                    }
+                }
+                else if (strcmp(args[j], "<") == 0)
+                {
+                    // hasRedir = 1;
+                    if (args[j + 1] == NULL || strcmp(args[j + 1], "ls") == 0 || strcmp(args[j + 1], "cat") == 0)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        int file = open(args[j + 1], O_RDONLY);
+                        if (file == -1)
+                        {
+                            printf("File doesn't exist in current directory");
+                            break;
+                        }
+                        dup2(file, STDIN_FILENO);
+                        close(file);
+                    }
+                }
+
+                else if (strcmp(args[j], ">") == 0)
+                {
+                    // hasRedir = 1;
+                    if (args[j + 1] == NULL || strcmp(args[j + 1], "ls") == 0 || strcmp(args[j + 1], "cat") == 0)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        int file = open(args[j + 1], O_CREAT | O_TRUNC | O_WRONLY);
+                        if (file == -1)
+                        {
+                            break;
+                        }
+                        dup2(file, STDOUT_FILENO);
+                        close(file);
+                    }
+                }
+
+                else if (strcmp(args[j], "2>") == 0)
+                {
+                    // hasRedir = 1;
+                    if (args[j + 1] == NULL || strcmp(args[j + 1], "ls") == 0 || strcmp(args[j + 1], "cat") == 0)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        int file = open(args[j + 1], O_CREAT | O_TRUNC | O_WRONLY);
+                        if (file == -1)
+                        {
+                            break;
+                        }
+                        dup2(file, STDERR_FILENO);
+                    }
+                }
+                else
+                {
+                    printf("");
+                }
+
+                if (strcmp(args[j], "|") == 0)
+                {
+                    int fd[2];
+                    if(pipe(fd) == -1){
+
+                    }
+
+                }
+                printf("%d",j);
             }
+            // printf("\n");
+            break;
         }
-        if (isRedirection == 1)
+        else
         {
-            redirect(input);
+            wait(NULL);
+            printf("\n");
         }
 
-        // Pipe
+        // list
+        // if (hasLs== 1 && hasRedir== 0)
+        // {
+        //     listFiles();
+        // }
+
+        // // Redirection
+        // if (hasLs== 1 && hasRedir== 1)
+        // {
+        //     redirect(args);
+        // }
+
+        // // Pipe
+        // if(hasPipe){
+        //     doPipe(args);
+        // }
     }
 
     return 0;
